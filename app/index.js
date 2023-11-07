a5f3412cbcfaafe4eef824914c31a786b72bcdb0
Ali Jahankah <aral2@yahoo.com>
2023-11-07 17:39:11 +0000

    feat-create table script and sql file

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 const server = require('./server')
+const { Client } = require('pg');
+const { DefaultAzureCredential } = require('@azure/identity');
 
 const init = async () => {
+  await executeSQLScript();
   await server.start()
   console.log('Server running on %s', server.info.uri)
 }
@@ -9,5 +12,36 @@ process.on('unhandledRejection', (err) => {
   console.log(err)
   process.exit(1)
 })
+
+async function executeSQLScript() {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+
+    if (process.env.NODE_ENV === 'production') {
+      const credential = new DefaultAzureCredential();
+      const accessToken = await credential.getToken('https://ossrdbms-aad.database.windows.net');
+      client.query(`SET SESSION AUTHORIZATION DEFAULT, PUBLIC, ${accessToken.token};`);
+    }
+
+    const fs = require('fs');
+    const sqlScript = fs.readFileSync('./sql/tables.sql', 'utf8');
+    await client.query(sqlScript);
+
+    console.log('Table creation script executed successfully.');
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    client.end();
+  }
+}
+
 
 init()
