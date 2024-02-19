e2fdb340bd3bfbe39329ad10b231339dc35ae5fe
Ali Jahankah <aral2@yahoo.com>
2024-02-19 16:06:43 +0000

    fix - comment sql code

--- app/index.js
+++ app/index.js
@@ -23,7 +23,7 @@ async function init () {
     await initializeMessageReceivers()
     await server.start()
     console.log('Server running on %s', server.info.uri)
-    await runSqlScript()
+    // await runSqlScript()
   } catch (error) {
     console.error('Error during initialization:', error)
     await cleanup()
@@ -59,46 +59,46 @@ async function initializeMessageReceivers () {
   await fileStoreReceiver.subscribe()
 }
 
-async function runSqlScript () {
-  const DB_NAME = process.env.POSTGRES_DB
-  const clientWithOutDb = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === environments.production
-  })
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === environments.production
-  })
-  try {
-    console.log('[CHECKING IF DB EXIST]')
-    await clientWithOutDb.connect()
-    const res = await clientWithOutDb.query(
-      `SELECT datname FROM pg_catalog.pg_database WHERE datname = '${DB_NAME}'`
-    )
-    if (res.rowCount === 0) {
-      console.log(`${DB_NAME} DATABASE NOT FOUND CREATING IT...`)
-      await clientWithOutDb.query(`CREATE DATABASE ${DB_NAME}`)
-      console.log(`YAHOO DB SUCCESSFULLY CREATED ${DB_NAME}.`)
-    }
-    await clientWithOutDb.end()
-    console.log('[TRYING TO READ SCRIPT FILE]')
-    const sqlScript = fs.readFileSync('./sql/tables.sql', 'utf8')
-    await client.connect()
-    await client.query(sqlScript)
-    console.log('Table creation script executed successfully.')
-  } catch (error) {
-    console.error('Error executing SQL script:', error)
-  } finally {
-    await client.end()
-  }
-}
+// async function runSqlScript () {
+//   const DB_NAME = process.env.POSTGRES_DB
+//   const clientWithOutDb = new Client({
+//     user: process.env.POSTGRES_USER,
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     password: process.env.POSTGRES_PASSWORD,
+//     ssl: process.env.NODE_ENV === environments.production
+//   })
+//   const client = new Client({
+//     user: process.env.POSTGRES_USER,
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
+//     ssl: process.env.NODE_ENV === environments.production
+//   })
+//   try {
+//     console.log('[CHECKING IF DB EXIST]')
+//     await clientWithOutDb.connect()
+//     const res = await clientWithOutDb.query(
+//       `SELECT datname FROM pg_catalog.pg_database WHERE datname = '${DB_NAME}'`
+//     )
+//     if (res.rowCount === 0) {
+//       console.log(`${DB_NAME} DATABASE NOT FOUND CREATING IT...`)
+//       await clientWithOutDb.query(`CREATE DATABASE ${DB_NAME}`)
+//       console.log(`YAHOO DB SUCCESSFULLY CREATED ${DB_NAME}.`)
+//     }
+//     await clientWithOutDb.end()
+//     console.log('[TRYING TO READ SCRIPT FILE]')
+//     const sqlScript = fs.readFileSync('./sql/tables.sql', 'utf8')
+//     await client.connect()
+//     await client.query(sqlScript)
+//     console.log('Table creation script executed successfully.')
+//   } catch (error) {
+//     console.error('Error executing SQL script:', error)
+//   } finally {
+//     await client.end()
+//   }
+// }
 
 async function cleanup () {
   await MessageSenders.closeAllConnections()
