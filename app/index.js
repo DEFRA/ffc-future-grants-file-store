71d980eb99536c6051c3799c230cc0413cb2b6dc
Ali Jahankah <aral2@yahoo.com>
2024-02-19 19:23:39 +0000

    fix comment local db

--- app/index.js
+++ app/index.js
@@ -5,11 +5,11 @@ const {
 const server = require('./server')
 
 async function init () {
-  const runSqlScript = require('./services/create-local-db')
+  // const runSqlScript = require('./services/create-local-db')
   await initializeMessageReceivers()
   await server.start()
   console.log('Server running on %s', server.info.uri)
-  await runSqlScript()
+  // await runSqlScript()
 }
 
 process.on('unhandledRejection', async (err) => {
